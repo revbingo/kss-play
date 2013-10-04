b92c0a9732c1b7a5b6574127e790073936e55c74
Mark Piper <mark.piper@wds.co>
2013-10-04 08:41:51 +0100

    MP: Tweaks

--- app/controllers/kssplay/Styleguide.java
+++ app/controllers/kssplay/Styleguide.java
@@ -1,14 +1,20 @@
 package controllers.kssplay;
 
+import java.lang.reflect.Modifier;
+import java.util.Set;
+
+import play.Play;
 import play.mvc.Controller;
+import play.vfs.VirtualFile;
 
 import com.revbingo.kss.KssParser;
 
 public class Styleguide extends Controller {
 
     public static void index() {
-    	KssParser parser = new KssParser("public/stylesheets");
-        render(parser);
+    	String stylesheetLocation = Play.configuration.getProperty("stylesheets.dir", "public/stylesheets");
+		KssParser parser = new KssParser(stylesheetLocation);
+		Set<String> sections = parser.getSections().keySet();
+        render(parser, sections);
     }
-
 }
