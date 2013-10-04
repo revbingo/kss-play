82a85fc75464045c849757028fe34cb4d81eb122
Mark Piper <mark.piper@wds.co>
2013-10-04 18:08:39 +0100

    MP: No need to pass parser around

--- app/controllers/kssplay/Styleguide.java
+++ app/controllers/kssplay/Styleguide.java
@@ -1,6 +1,7 @@
 package controllers.kssplay;
 
 import java.lang.reflect.Modifier;
+import java.util.Map;
 import java.util.Set;
 
 import play.Play;
@@ -8,13 +9,15 @@ import play.mvc.Controller;
 import play.vfs.VirtualFile;
 
 import com.revbingo.kss.KssParser;
+import com.revbingo.kss.Section;
 
 public class Styleguide extends Controller {
 
     public static void index() {
     	String stylesheetLocation = Play.configuration.getProperty("stylesheets.dir", "public/stylesheets");
 		KssParser parser = new KssParser(stylesheetLocation);
-		Set<String> sections = parser.getSections().keySet();
-        render(parser, sections);
+		Map<String, Section> sections = parser.getSections();
+		Set<String> sectionList = parser.getSections().keySet();
+        render(sectionList, sections);
     }
 }
