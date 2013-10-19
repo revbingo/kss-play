1dbbd935420cc6b8515ceb193fc0045928d7a42e
Mark Piper <mark.piper@wds.co>
2013-10-19 12:05:34 +0100

    MP: Add example project

---
+++ example/kss-generator/app/controllers/Application.java
@@ -0,0 +1,12 @@
+package controllers;
+
+import play.Play;
+import play.Play.Mode;
+import play.mvc.Controller;
+
+public class Application extends Controller {
+
+	public static void index() {
+		render();
+	}
+}
